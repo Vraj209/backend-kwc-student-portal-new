36d9dfdc551a99618710d1862919e16e15cd12eb
vraj <vrajpanchal209@gmail.com>
2025-01-20 18:34:15 -0500

    index.ts change

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("welcome to student portal");
+  res.send("welcome to student portal baps student");
 });
 
 app.listen(process.env.BACKEND_PORT, () => {
